d3744e74379bc5dbc4218797f6fb99f26f9b4123
Osben Toulsongit <otoulson@dcscorp.com>
2022-08-05 17:51:13 -0400

    Console

---
+++ Console.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DebugTools
+{
+    public static class DebugConsole
+    {
+        public static void WriteToConsole(string message)
+        {
+#if DEBUG
+            System.Diagnostics.Debug.WriteLine(message);
+#endif
+            System.Console.WriteLine(message);
+
+        }
+    }
+}
